b0b45ead4cd0703fca88474d4a1e8d141c7b4ac1
Kween <kweenyoallhiada@gmail.com>
2023-01-17 22:05:19 -0800

    Update counter logic

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,19 +1,47 @@
-// document.getElementById("count-el").innerText = 5;
+/*
+initialize count to 0
+listen for clicks on the increment button
+change the count-el in the HTML to reflect the new count
+*/
+let countEl = document.getElementById("count-el").innerText = 0;
+let count = 0;
+function increment() {
+    count += 1;
+    console.log(count);
+}
 
-// let count = 0;
 
-// Create a variable, bonusPoints. 
-// Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase 
-// it to 70
-// Console.log the value after each step
+// addEventListener("click", "count-el");
 
-let bonusPoints = 50;
-console.log(bonusPoints)
-bonusPoints += 50;
-console.log(bonusPoints)
-bonusPoints -= 75;
-console.log(bonusPoints)
-bonusPoints += 45;
-console.log(bonusPoints)
 
+// function increment() {
+//  count += 1;
+// }
+
+// increment();
+
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+// Create a function that logs out the sum
+// of all the lap times
+
+// function lapTimeSum() {
+//     let totalTime = lap1 + lap2 + lap3;
+//     console.log(totalTime);
+// }
+
+// lapTimeSum();
+
+// let lapsCompleted = 0;
+
+// function oneAdditionalLap() {
+//     lapsCompleted += 1;
+// }
+
+// oneAdditionalLap();
+// oneAdditionalLap();
+// oneAdditionalLap();
+
+// console.log(lapsCompleted);
