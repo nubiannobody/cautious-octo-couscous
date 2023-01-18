1607bdabced1652d13a4ccfca8720fbecdd7e281
Kween <kweenyoallhiada@gmail.com>
2023-01-17 20:57:12 -0800

    Add a button

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,2 +1,19 @@
-document.getElementById("count-el").innerText = 5;
+// document.getElementById("count-el").innerText = 5;
+
+// let count = 0;
+
+// Create a variable, bonusPoints. 
+// Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase 
+// it to 70
+// Console.log the value after each step
+
+let bonusPoints = 50;
+console.log(bonusPoints)
+bonusPoints += 50;
+console.log(bonusPoints)
+bonusPoints -= 75;
+console.log(bonusPoints)
+bonusPoints += 45;
+console.log(bonusPoints)
 
