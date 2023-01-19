e826ce814db53daf7287dfabfc2ab74323ea14c4
Kween <kweenyoallhiada@gmail.com>
2023-01-19 02:14:39 -0800

    Add challenge answer

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -53,3 +53,15 @@ save();
 // oneAdditionalLap();
 
 // console.log(lapsCompleted);
+
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+// let welcomeEl = document.getElementById("welcome-el")
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+
+// let fullName = "Kween Yoallhiada Karrielene"
+// let greeting = "Welcome back, "
+
+// Render the welcome message using welcomeEl.innerText
+// welcomeEl.innerText = greeting + fullName;
